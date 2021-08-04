a50c02d3ced949d3ca55a88ddb01fe4aa1653720
Ricardo Sutil <ricardosutil@gmail.com>
2021-08-03 22:12:24 -0300

    Service layer, component registration

--- src/main/java/com/sutil/ricardo/course/entities/User.java
+++ src/main/java/com/sutil/ricardo/course/entities/User.java
@@ -1,14 +1,21 @@
 package com.sutil.ricardo.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	/**
 	 * 
@@ -22,6 +29,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -74,6 +85,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
